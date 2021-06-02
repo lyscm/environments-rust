d717408ec47c34f60b2b39eab36ebd17a8712e6e
Q-CTeq <vsc-environment-builder@qcteq.com>
2021-06-02 23:11:07 +0000

    commit change

--- image-init.sh
+++ image-init.sh
@@ -2,18 +2,15 @@
 docker login -u qcteqcr
 
 # Set variables
-TAG_PUSH=qcteqcr/rust-development-environment
-TAG_BUILDER=qcteqcr/rust-environment-builder
+TAG=qcteqcr/rust-development-environment
 
 # Copy extensions
 cp -r $HOME/.vscode-server/extensions/* ./.vscode-extensions
 
-docker tag $TAG_BUILDER $TAG_PUSH
-
 # Build repository images
-docker buildx build --push --platform=linux/amd64,linux/arm64,linux/arm/v7 -t $TAG_PUSH . 
+docker buildx build --push --platform=linux/amd64,linux/arm64,linux/arm/v7 -t $TAG . 
 
 # Remove extensions
 rm -r ./.vscode-extensions/**/
 
-docker run -d --restart unless-stopped -v /var/run/docker.sock:/var/run/docker-host.sock $TAG_PUSH
+docker run -d --restart unless-stopped -v /var/run/docker.sock:/var/run/docker-host.sock $TAG
